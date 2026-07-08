5b534d91518fd674a639924e7795373480cf6ba4
JasdeepChahal6 <jasdeep20045@gmail.com>
2026-07-08 11:37:12 -0400

    intentional failing test

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from FastAPI!"}
+    assert response.json() == {"message": "WRONG MESSAGE!"}
 
 def test_health():
     response = client.get("/health")
